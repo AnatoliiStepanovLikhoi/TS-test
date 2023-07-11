c16475e452ffb14c12231952de418f80e1d2b562
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-11 22:31:41 +0300

    add: functions declaration

--- src/parts/4-functions.ts
+++ src/parts/4-functions.ts
@@ -16,7 +16,7 @@ interface MyPositionWithDefault extends MyPosition {
 }
 
 function position(): MyPosition;
-function position(a: number): MyPositionWithDefault;
+// function position(a: number): MyPositionWithDefault;
 function position(a: number, b: number): MyPosition;
 
 function position(a?: number, b?: number) {
@@ -32,5 +32,31 @@ function position(a?: number, b?: number) {
 }
 
 console.log(position());
-console.log(position(42));
+// console.log(position(42));
 console.log(position(10, 15));
+
+//FUNCTIONS
+
+let sayHi = () => {
+  console.log("Hi, welcome");
+};
+
+// sayHi();
+
+let functionReturnString = (): string => {
+  return "typescript";
+};
+
+let multiple = (num: number) => {
+  return num * 2;
+};
+
+// let multiple2 = (num: number):void => {
+//   return num * 2;
+// };
+
+let sum = (num1: number, num2: number, another?: number) => {
+  return num1 + num2;
+};
+
+console.log(sum(2, 3));
