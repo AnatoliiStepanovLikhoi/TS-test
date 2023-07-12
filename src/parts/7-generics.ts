d6a8cfd3be052bde3a8c9ecc21daa979a1ccea74
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-12 19:10:29 +0300

    add: generics

--- src/parts/7-generics.ts
+++ src/parts/7-generics.ts
@@ -8,18 +8,18 @@ function reverse<T>(array: T[]): T[] {
 console.log(reverse(arrayOfNumbers));
 reverse(arrayOfStrings);
 
-class Flight<const T> {
-  constructor(private dest: T[]) {}
+// class Flight<const T> {
+//   constructor(private dest: T[]) {}
 
-  @test
-  fly(to: T) {}
+//   @test
+//   fly(to: T) {}
 
-  @test
-  fly2(to: T) {}
-}
+//   @test
+//   fly2(to: T) {}
+// }
 
-const flight = new Flight(["GB", "UA"]);
-flight.fly("UA");
+// const flight = new Flight(["GB", "UA"]);
+// flight.fly("UA");
 
 function test<This, Args extends any[], Return>(
   target: (this: This, ...args: Args) => Return,
@@ -36,3 +36,57 @@ function test<This, Args extends any[], Return>(
     return res;
   };
 }
+
+//Generics
+
+interface iAuthor {
+  id: number;
+  name: string;
+}
+
+interface iCategory {
+  id: number;
+  title: string;
+}
+
+interface IPost {
+  id: number;
+  title: string;
+  desc: string;
+  extra: iAuthor[] | iCategory[];
+}
+
+interface IPostBetter<T> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+const testMe: IPostBetter<string> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: ["str", "str2"],
+};
+
+interface IPostEvenBetter<T extends object> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+const testMe2: IPostEvenBetter<iAuthor> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: [{ id: 1, name: "john" }],
+};
+
+const testMe3: IPostEvenBetter<iCategory> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: [{ id: 1, title: "category" }],
+};
